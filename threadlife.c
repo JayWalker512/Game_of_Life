8843287043b9c9965f0971072a3bdaa749baf373
Brandon Foltz <bsfoltzguitar@gmail.com>
2013-10-13 15:14:06 -0400

    Fixed indentation in threadlife.c and remove extraneous printf lines.

--- threadlife.c
+++ threadlife.c
@@ -9,26 +9,20 @@ void *ThreadLifeMain(void *worldContext)
 {
 	ThreadWorldContext_t *context = worldContext;
 
-	printf("World dims are (%ld, %ld)\n", 
-		context->front->width,
-		context->front->height);
-
 	unsigned long generations = 0;
 	while (context->bRunning)
 	{
-      LifeGeneration(context->back, context->front);
-      SwapThreadWorldContextPointers(context);
-      generations = DoGensPerSec(generations);
+    LifeGeneration(context->back, context->front);
+    SwapThreadWorldContextPointers(context);
+    generations = DoGensPerSec(generations);
 
-      if (context->bRandomize)
-      {
-      	pthread_mutex_lock(&context->lock);
-      	context->bRandomize = 0;
-      	RandomizeWorldStateBinary(context);
-      	pthread_mutex_unlock(&context->lock);
-      }
-	
+    if (context->bRandomize)
+    {
+    	pthread_mutex_lock(&context->lock);
+    	context->bRandomize = 0;
+    	RandomizeWorldStateBinary(context);
+    	pthread_mutex_unlock(&context->lock);
+    }
 	}
-
 	return NULL;
 }
