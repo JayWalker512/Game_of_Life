91b7edf29a8ef171f0542659a44b10fd1cb7eb08
Brandon Foltz <bsfoltzguitar@gmail.com>
2014-04-07 19:10:32 -0400

    Updated todo with a known bug. Noted this bug in the determinism_test.sh as it is related.

--- src/determinism_test.sh
+++ src/determinism_test.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 #Determinism test! 
+#NOTE: COPY SCRIPT TO BINARY DIR BEFORE RUNNING TO AVOID SEGFAULT
 #We compare the output of infinite GoL simulations to a known-good* master file
 #until (if ever) we get an output that doesn't match the master file. This signals
 #a bug causing a break in determinism.
+BINDIR="../"
+DATADIR="../data/"
+
+if [ ! -e gameoflife ]; then
+	echo "Copy this script to the dir containing \"gameoflife\" binary before running!"
+	exit
+fi
+
 if [ "$1" == "" ]; then
 	threads="2"
 else
@@ -10,7 +19,7 @@ else
 fi
 
 #generate the control
-./gameoflife -l data/r-pentomino.life -o control.life -g 15000 -h 304 -t "$threads" > /dev/null
+gameoflife -l data/r-pentomino.life -o control.life -g 15000 -h 304 -t "$threads" > /dev/null
 echo "Created control.life file."
 
 totalIterations=1000
